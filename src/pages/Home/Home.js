4d73dbdee4db921bdca03f78ee089b85a175aaa2
Michalis Merkouris <mimerkou@gmail.com>
2021-12-02 23:51:58 +0200

    finished team section in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Button from '../../components/Button/Button';
 import homeAboutImg from '../../assets/kentro-f-03.jpg';
+import toliosPhoto from '../../assets/team-tolios.jpg';
+import stavropoulouPhoto from '../../assets/team-stavropoulou.jpg';
+import kourikaPhoto from '../../assets/team-kourika.jpg';
+import platanitiPhoto from '../../assets/team-plataniti.jpg';
 import './Home.css';
 
 const Home = () => {
@@ -53,6 +57,39 @@ const Home = () => {
                 <img src={homeAboutImg} alt="About" />
               </div>
             </div>
+          </div>
+        </div>
+
+        <div className="team-container">
+          <h2>Η Ομάδα μας</h2>
+
+          <div className="team-wrapper">
+            <div className="team-info">
+              <img src={toliosPhoto} alt="Αθανάσιος Τόλιος" />
+              <h4>Αθανάσιος Τόλιος</h4>
+              <p>MSc Κλινικής Ψυχολογίας</p>
+            </div>
+            <div className="team-info">
+              <img src={stavropoulouPhoto} alt="Μάρθα Σταυροπούλου" />
+              <h4>Μάρθα Σταυροπούλου</h4>
+              <p>Κοινωνική Λειτουργός</p>
+            </div>
+            <div className="team-info">
+              <img src={kourikaPhoto} alt="Μαριέττα Κουρίκα" />
+              <h4>Μαριέττα Κουρίκα</h4>
+              <p>Ψυχολόγος</p>
+            </div>
+            <div className="team-info">
+              <img src={platanitiPhoto} alt="Παναγιώτα Πλατανίτη" />
+              <h4>Παναγιώτα Πλατανίτη</h4>
+              <p>Ψυχολόγος</p>
+            </div>
+          </div>
+
+          <div className="team-btn-wrap">
+            <Button path="/our-team" darkBg={false}>
+              Βιογραφικά
+            </Button>
           </div>
         </div>
       </div>
