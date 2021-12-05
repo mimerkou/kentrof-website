0b4406bf37d0c130a66fe6e831527f30f2b63661
Michalis Merkouris <mimerkou@gmail.com>
2021-12-05 10:47:16 +0200

    installed react-leaflet package to display map on home-page

---
+++ src/components/Home-Sections/HomeMap/HomeMap.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HomeMap.css';
+
+const HomeMap = () => {
+  const position = [37.906248, 23.742208]; // Τιτάνων 23, Ελληνικό Αττικής
+
+  return <div className="home-map-container"></div>;
+};
+
+export default HomeMap;
