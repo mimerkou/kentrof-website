34e316edeeb41b7091d7eec261ef78ad3b300fa6
Michalis Merkouris <mimerkou@gmail.com>
2021-12-08 19:42:06 +0200

    added scroll to top on page changed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import HomeBanner from '../../components/Home-Sections/HomeBanner/HomeBanner';
 import HomeAbout from '../../components/Home-Sections/HomeAbout/HomeAbout';
 import HomeTeam from '../../components/Home-Sections/HomeTeam/HomeTeam';
@@ -8,6 +9,12 @@ import HomeMap from '../../components/Home-Sections/HomeMap/HomeMap';
 import './Home.css';
 
 const Home = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <div className="home-container">
