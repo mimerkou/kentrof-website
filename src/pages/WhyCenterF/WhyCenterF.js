2dbd5e8f74a5f6095c72e5aa940b111568d59e3b
Michalis Merkouris <mimerkou@gmail.com>
2021-12-09 20:54:58 +0200

    started working on why-center-f page

---
+++ src/pages/WhyCenterF/WhyCenterF.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WhyCenterF.css';
+
+const WhyCenterF = () => {
+  return (
+    <div className="why-center-f-container">
+      <div className="why-center-f-wrapper"></div>
+    </div>
+  );
+};
+
+export default WhyCenterF;
