d0e374418ac6d3c6cfc3e3161c48ce50f1835f60
Michalis Merkouris <mimerkou@gmail.com>
2021-12-12 21:54:41 +0200

    started working on donation page

---
+++ src/pages/Donation/Donation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Donation.css';
+
+const Donation = () => {
+  return (
+    <div className="donation-container">
+      <div className="donation-wrapper"></div>
+    </div>
+  );
+};
+
+export default Donation;
