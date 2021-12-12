e5035bc5d5fe11252c9d6f23a92558ff2f2165a1
Michalis Merkouris <mimerkou@gmail.com>
2021-12-12 22:26:06 +0200

    started working on volunteering page

---
+++ src/pages/Volunteering/Volunteering.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Volunteering.css';
+
+const Volunteering = () => {
+  return (
+    <div className="volunteering-container">
+      <div className="volunteering-wrapper">
+        <div className="volunteering-box"></div>
+
+        <div className="volunteering-box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Volunteering;
